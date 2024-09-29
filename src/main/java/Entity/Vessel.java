8cd0daa70b65abbb04e85ae2b00aa567585ecd6f
TYJ <tanyj-wm23@student.tarc.edu.my>
2024-09-29 21:35:43 +0800

    commit test

--- src/main/java/Entity/Vessel.java
+++ src/main/java/Entity/Vessel.java
@@ -2,4 +2,43 @@ package Entity;
 
 public class Vessel {
 
+    private int vesselID;
+    private String vesselName;
+    private String vesselAddress;
+    private String vesselPhone;
+    public Vessel() {
+
+    }
+
+    public int getVesselID() {
+        return vesselID;
+    }
+
+    public void setVesselID(int vesselID) {
+        this.vesselID = vesselID;
+    }
+
+    public String getVesselName() {
+        return vesselName;
+    }
+
+    public void setVesselName(String vesselName) {
+        this.vesselName = vesselName;
+    }
+
+    public String getVesselAddress() {
+        return vesselAddress;
+    }
+
+    public void setVesselAddress(String vesselAddress) {
+        this.vesselAddress = vesselAddress;
+    }
+
+    public String getVesselPhone() {
+        return vesselPhone;
+    }
+
+    public void setVesselPhone(String vesselPhone) {
+        this.vesselPhone = vesselPhone;
+    }
 }
